3a36b76fae8ec0496784f6484469256904aee13c
Nolan Kovacik <noltron000@gmail.com>
2018-11-27 10:02:48 -0800

    fixing handlebars - home view works

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-let exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const port = 3000;
@@ -9,13 +10,13 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(express.static('public'));
-app.use(methodOverride('_method'));
+// app.use(express.static('public'));
+// app.use(methodOverride('_method'));
 
 
 
 app.get('/', (req, res) => {
-	res.render('main')
+	res.render('home')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
