91550d3b19a32ae13d77faee18023c339fb81803
Nolan Kovacik <noltron000@gmail.com>
2018-11-27 13:22:42 -0800

    trying to fix this gosh darned mongodb"

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
 	app.post('/posts/new', (req, res) => {
 		// INSTANTIATE INSTANCE OF POST MODEL
 		const post = new Post(req.body);
-
 		// SAVE INSTANCE OF POST MODEL TO DB
 		post.save((err, post) => {
 			// REDIRECT TO THE ROOT
