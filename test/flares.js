0e538d688469c05236c0056901bc90cd34051682
Nolan Kovacik <noltron000@gmail.com>
2018-11-29 10:28:31 -0800

    adding flare test

---
+++ test/flares.js
@@ -0,0 +1,28 @@
+const Flare = require('../models/flare');
+
+const flare = {
+	title: "flare title",
+	url: "https://www.google.com",
+	summary: "flare summary"
+};
+
+Flare.findOneAndRemove(flare, () => {
+	Flares.find((err, flares) => {
+		let flareCount = flares.count;
+
+		chai
+			.request("localhost:3000")
+			.post("/flares/new")
+			.send(flare)
+			.then(res => {
+				Flare.find((err, flares) => {
+					flareCount.should.be.equal(flares.length - 1);
+					res.should.have.status(200);
+					return done();
+				});
+			})
+			.catch(err => {
+				return done(err);
+			});
+	});
+})
