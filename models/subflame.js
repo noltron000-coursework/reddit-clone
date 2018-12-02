d48511471b26169af3dc7767a52282a4d88807c7
Nolan Kovacik <noltron000@gmail.com>
2018-12-02 12:03:45 -0800

    URL-Friendly Title created

--- models/subflame.js
+++ models/subflame.js
@@ -6,6 +6,10 @@ const SubFlameSchema = new Schema({
 		type: String,
 		required: true
 	},
+	urlFriendlyTitle: {
+		type: String,
+		required: true
+	},
 	synopsis: { // synopsis implies a short body text
 		type: String,
 		required: true
@@ -15,17 +19,37 @@ const SubFlameSchema = new Schema({
 	},
 	updation: { // updation is like creation
 		type: Date
-	}
+	},
 });
 
 SubFlameSchema.pre("save", (next) => {
-	// SET createdAt AND updatedAt
+	// SET creation AND updation
 	const now = new Date();
 	this.updation = now;
 
 	if (!this.createdAt) {
 		this.creation = now;
 	}
+
+	// Generate a URL Friendly Title
+	let string = this.title.toLowerCase();
+	let newString = '';
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz-1234567890';
+
+	for (let char = 0; char <= string.length; char += 1) {
+		// loop through the string
+		if (alphabet.includes(char)) {
+			// check if it is a letter number or hyphen
+			//	 if so, add char to newString
+			newString += char;
+		} else if (' _'.includes(char)) {
+			// else, check if it is a space or underscore
+			//	 if so, add hyphen to newString
+			newString += '-';
+		}
+		// else, drop character by not appending it
+	} // slug
+	this.urlFriendlyTitle = newString;
 
 	next();
 });
