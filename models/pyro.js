6615df77e400c132a53fb9494a5bdb7ca3087d05
Nolan Kovacik <noltron000@gmail.com>
2018-12-02 15:47:01 -0800

    encrypting pyro model password

--- models/pyro.js
+++ models/pyro.js
@@ -32,7 +32,25 @@ PyroSchema.pre("save", (next) => {
 		this.creation = now;
 	}
 
-	next();
+
+	// ENCRYPT PASSWORD
+	const user = this;
+	if (!user.isModified("password")) {
+		return next();
+	}
+	bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			user.password = hash;
+			next();
+		});
+	});
 });
+
+// Need to use function to enable this.password to work.
+UserSchema.methods.comparePassword = function (password, done) {
+	bcrypt.compare(password, this.password, (err, isMatch) => {
+		done(err, isMatch);
+	});
+};
 
 module.exports = mongoose.model("Pyro", PyroSchema);
