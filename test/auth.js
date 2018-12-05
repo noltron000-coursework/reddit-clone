9d7b2e638558a2dc4e4c08c09ca54a032d299a3b
Nolan Kovacik <noltron000@gmail.com>
2018-12-05 15:54:55 -0800

    passing two tests now

--- test/auth.js
+++ test/auth.js
@@ -1,17 +1,21 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../server");
-const should = chai.should();
 chai.use(chaiHttp);
+const should = chai.should();
 
-const agent = chai.request.agent(server);
+const server = require("../server");
+const agent = chai.request.agent('http://localhost:3000');
 const Pyro = require("../models/pyro");
 
-// describe("Pyro", function () {
-// 	it("should not be able to login if they have not registered", done => {
-// 		agent.post("/login", { email: "wrong@wrong.com", password: "nope" }).end(function (err, res) {
-// 			res.status.should.be.equal(401);
-// 			done();
-// 		});
-// 	});
-// });
+
+describe("Pyro", function () {
+	it("should not be able to login if they have not registered", (done) => {
+		agent.post("/login", {
+			email: "wrong@wrong.com",
+			password: "nope"
+		}).end(function (err, res) {
+			res.status.should.be.equal(401);
+			done()
+		});
+	});
+});
