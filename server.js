ca2dbcf3c0b76a24463898810df2d51236bff40f
Nolan Kovacik <noltron000@gmail.com>
2019-05-30 11:20:34 -0700

    adding clickable link to dev environment

--- server.js
+++ server.js
@@ -82,5 +82,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}!`)
+	console.log(`Example app listening on http://localhost:${port}/ !`)
 })
